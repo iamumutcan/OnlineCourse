84075bb3f5ff7c94b43b1904a58a019b97667331
Umut Can YILMAZ <umut31can@gmail.com>
2024-03-15 00:32:29 +0300

    [Feat] : Added entities

---
+++ src/onlineCourse/Domain/Entities/CourseContent.cs
@@ -0,0 +1,13 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System.Text.Json.Serialization;
+
+namespace Domain.Entities;
+
+public class CourseContent : Entity<Guid>
+{
+    public string Summary { get; set; }
+    public Guid CourseId { get; set; }
+    [JsonIgnore]
+    public virtual Course Course { get; set; }
+    public virtual CourseDocument CourseDocument { get; set; }
+}
---
+++ src/onlineCourse/Domain/Entities/CourseDocument.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+using NArchitecture.Core.Persistence.Repositories;
+using System.Text.Json.Serialization;
+
+namespace Domain.Entities;
+
+public class CourseDocument : Entity<Guid>
+{
+    public string FileName { get; set; }
+    public string FileExtension { get; set; }
+    public  ContentType Type { get; set; }
+    public int Duration { get; set; }
+    public double FileSize { get; set; }
+    public Guid CourseContentId { get; set; }
+    [JsonIgnore]
+    public virtual CourseContent CourseContent { get; set; }
+
+}
